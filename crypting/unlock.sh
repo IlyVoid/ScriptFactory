482c9639e7223d7581d6fd539597b8ef4fe58243
Quinten Van Den Berghe <quvan-de@a1-r1-p3.s19.be>
2024-09-18 14:55:36 +0200

    small update

--- crypting/unlock.sh
+++ crypting/unlock.sh
@@ -8,49 +8,46 @@ NC='\033[0m' # No Color
 
 # Decrypt function
 decrypt() {
-    local key="$1"
-    local encryptedFile="$2"
-    local outFile="${encryptedFile%.encrypted}"
+    local encryptedFile="$1"
     local IVFile="$encryptedFile.iv"
     local IV=""
-    local fileSize=0
+    local password
+    local key
 
     # Read IV from IV file
     IV=$(cat "$IVFile")
     # Remove IV file
     rm -f "$IVFile"
 
-    # Get file size
-    IFS= read -r -n 16 fileSize < "$encryptedFile"
+    # Prompt for passphrase
+    printf "${BLUE}Enter the passphrase: ${NC}\n"
+    read -rs password
+    key="$(echo -n "$password" | sha256sum | cut -d" " -f1)"
 
-    # Decrypt file
-    openssl enc -aes-256-cbc -K "$key" -iv "$IV" -d -in "$encryptedFile" -out "$outFile.tmp"
+    # Attempt to decrypt file
+    openssl enc -aes-256-cbc -K "$key" -iv "$IV" -d -in "$encryptedFile" -out "${encryptedFile%.encrypted}.tmp"
 
     # Check if decryption was successful
     if [ $? -eq 0 ]; then
-        mv "$outFile.tmp" "$outFile" # Rename decrypted file
-        printf "${GREEN}Decrypted: $outFile${NC}\n"
+        mv "${encryptedFile%.encrypted}.tmp" "${encryptedFile%.encrypted}" # Rename decrypted file
+        printf "${GREEN}Decrypted: ${encryptedFile%.encrypted}${NC}\n"
+        rm -f "$encryptedFile" # Remove the encrypted file after successful decryption
     else
-        printf "${RED}Failed to decrypt: $encryptedFile${NC}\n"
-		rm -rf "$outFile.tmp"
+        printf "${RED}Failed to decrypt: $encryptedFile. Keeping the encrypted file.${NC}\n"
+        rm -f "${encryptedFile%.encrypted}.tmp" # Clean up any temporary files created during decryption
     fi
-
-    # Remove encrypted file
-    rm -f "$encryptedFile"
 }
 
 # Main function
 main() {
-    local password
-
-    echo -e "${BLUE}Enter the passphrase: ${NC}"
-    read -rs password
-
     # Decrypt all encrypted files in the current directory and subdirectories
     for file in $(find . -type f -name "*.encrypted"); do
-        decrypt "$(echo -n "$password" | sha256sum | cut -d" " -f1)" "$file"
+        decrypt "$file"
     done
+
+    printf "${GREEN}Decryption process completed.${NC}\n"
 }
 
 # Call the main function
 main
+
