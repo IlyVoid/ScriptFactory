28f56498356fa2e1eadb0bd290dd3600af600b83
Quinten Van Den Berghe <quvan-de@a1-r1-p3.s19.be>
2024-09-18 15:00:23 +0200

    updated and functional

--- crypting/unlock.sh
+++ crypting/unlock.sh
@@ -13,29 +13,44 @@ decrypt() {
     local IV=""
     local password
     local key
+    local success=0
 
     # Read IV from IV file
     IV=$(cat "$IVFile")
-    # Remove IV file
-    rm -f "$IVFile"
 
-    # Prompt for passphrase
-    printf "${BLUE}Enter the passphrase: ${NC}\n"
-    read -rs password
-    key="$(echo -n "$password" | sha256sum | cut -d" " -f1)"
+    while true; do
+        # Prompt for passphrase
+        printf "${BLUE}Enter the passphrase for $encryptedFile: ${NC}\n"
+        read -rs password
+        key="$(echo -n "$password" | sha256sum | cut -d" " -f1)"
 
-    # Attempt to decrypt file
-    openssl enc -aes-256-cbc -K "$key" -iv "$IV" -d -in "$encryptedFile" -out "${encryptedFile%.encrypted}.tmp"
+        # Attempt to decrypt file
+        openssl enc -aes-256-cbc -K "$key" -iv "$IV" -d -in "$encryptedFile" -out "${encryptedFile%.encrypted}.tmp"
 
-    # Check if decryption was successful
-    if [ $? -eq 0 ]; then
-        mv "${encryptedFile%.encrypted}.tmp" "${encryptedFile%.encrypted}" # Rename decrypted file
-        printf "${GREEN}Decrypted: ${encryptedFile%.encrypted}${NC}\n"
-        rm -f "$encryptedFile" # Remove the encrypted file after successful decryption
-    else
-        printf "${RED}Failed to decrypt: $encryptedFile. Keeping the encrypted file.${NC}\n"
-        rm -f "${encryptedFile%.encrypted}.tmp" # Clean up any temporary files created during decryption
-    fi
+        # Check if decryption was successful
+        if [ $? -eq 0 ]; then
+            mv "${encryptedFile%.encrypted}.tmp" "${encryptedFile%.encrypted}" # Rename decrypted file
+            printf "${GREEN}Successfully decrypted: ${encryptedFile%.encrypted}${NC}\n"
+            rm -f "$encryptedFile" # Remove the encrypted file after successful decryption
+			rm -rf "$IVFile"
+            success=1
+            break
+        else
+            printf "${RED}Failed to decrypt: $encryptedFile. Please try again.${NC}\n"
+            rm -f "${encryptedFile%.encrypted}.tmp" # Clean up any temporary files created during decryption
+
+            # Ask if the user wants to continue trying or exit
+            printf "${BLUE}Would you like to try again? (y/n): ${NC}"
+            read -r answer
+
+            if [[ "$answer" != "y" ]]; then
+                printf "${RED}Keeping the encrypted file: $encryptedFile.${NC}\n"
+                break
+            fi
+        fi
+    done
+
+    return $success
 }
 
 # Main function
